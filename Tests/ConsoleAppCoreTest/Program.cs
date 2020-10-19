6555d0fcee88fbc1ec311942c87d3af730752a5b
Uladzislau Vasilyeu <vasilyeu.uv@mail.ru>
2020-10-19 20:38:56 +0300

    Тестовый консольный проект

---
+++ Tests/ConsoleAppCoreTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppCoreTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
