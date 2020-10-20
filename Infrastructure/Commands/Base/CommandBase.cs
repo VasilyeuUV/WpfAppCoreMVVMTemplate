b2836988f44e99427a4101cfdf8c89cb3618da36
Uladzislau Vasilyeu <vasilyeu.uv@mail.ru>
2020-10-20 17:27:40 +0300

    Базовый класс (абстрактный) команды

---
+++ Infrastructure/Commands/Base/CommandBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfAppCoreMVVMTemplate.Infrastructure.Commands.Base
+{
+    internal abstract class CommandBase : ICommand
+    {
+        public event EventHandler CanExecuteChanged    // событие, когда команда переходит из одного состояния в другое
+        {                                              // реализуем событие явно для передачи этого события под управление WPF
+            add => CommandManager.RequerySuggested += value;    // подписываемся на обработчик события
+            remove => CommandManager.RequerySuggested -= value; // отписываемся
+        }
+
+        /// <summary>
+        /// Можно или нет выполнить команду
+        /// (включает/выключает визуальный элемент с командой)
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public abstract bool CanExecute(object parameter);          // реализацией займется наследник
+
+
+        /// <summary>
+        /// ТО, что должно быть выполнено командой (основная логика команды)
+        /// </summary>
+        /// <param name="parameter"></param>
+        public abstract void Execute(object parameter);
+    }
+}
