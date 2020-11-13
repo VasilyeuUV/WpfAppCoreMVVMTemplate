69d28b4dd7606fecc8669ba20e3316b00cab5249
Uladzislau Vasilyeu <vasilyeu.uv@mail.ru>
2020-11-13 05:14:35 +0300

    Новая точка входа в приложение - класс Program

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfAppCoreMVVMTemplate
+{
+    public static class Program
+    {
+        /// <summary>
+        /// Application Entry Point.
+        /// </summary>        
+        //[System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        //[System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.STAThreadAttribute()]
+        public static void Main()
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
