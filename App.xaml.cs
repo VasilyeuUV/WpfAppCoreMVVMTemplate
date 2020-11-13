a2e2542e610612a4036e428357c5a7fa91d0fc05
Uladzislau Vasilyeu <vasilyeu.uv@mail.ru>
2020-11-13 14:00:59 +0300

    создание хоста

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 using System.Windows;
 using WpfAppCoreMVVMTemplate.ViewModels;
 
@@ -10,6 +11,23 @@ namespace WpfAppCoreMVVMTemplate
     /// </summary>
     public partial class App : Application
     {
+
+        #region HOST
+
+        private static IHost _host;
+
+        /// <summary>
+        /// Хост
+        /// </summary>
+        public static IHost Host => _host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        #endregion
+
+        /// <summary>
+        /// Подключение сервисов
+        /// </summary>
+        /// <param name="host"></param>
+        /// <param name="services"></param>
         internal static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
         {
             services.AddSingleton<MainWindowViewModel>();   // ViewModel главного окна
