5ec30f2c747c825ebddf081613a0537aaa2863d9
Uladzislau Vasilyeu <vasilyeu.uv@mail.ru>
2020-11-14 23:13:36 +0300

    Преобразование ViewModel для использования в IoC+DI

---
+++ ViewModels/ViewModelLocator.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace WpfAppCoreMVVMTemplate.ViewModels
+{
+    /// <summary>
+    /// Позволяет осуществить доступ к конкретным ViewModel
+    /// </summary>
+    internal class ViewModelLocator
+    {
+        /// <summary>
+        /// Свойство для получения ViewModel главного окна приложения
+        /// </summary>
+        public MainWindowViewModel MainWindowViewModel => App.Host.Services.GetRequiredService<MainWindowViewModel>();
+
+    }
+}
