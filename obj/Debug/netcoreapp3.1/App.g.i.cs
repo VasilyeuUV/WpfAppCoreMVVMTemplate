bc79de5bab0e8e1e164d00bbbc793b298d96efa0
Uladzislau Vasilyeu <vasilyeu.uv@mail.ru>
2020-11-15 04:05:11 +0300

    poolrequest to dev

--- obj/Debug/netcoreapp3.1/App.g.i.cs
+++ obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -60,7 +60,7 @@ namespace WpfAppCoreMVVMTemplate {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/WpfAppCoreMVVMTemplate;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfAppCoreMVVMTemplate;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
